a41b66bf8cb7b130c16560c04adb96bfb9453c64
Luan-nee <luandelsol54@gmail.com>
2024-09-01 12:30:12 -0500

    agregando nuevo diseño a interfaz de productos

--- view/productos.php
+++ view/productos.php
@@ -9,40 +9,57 @@ include_once("view/layout/header.php");
   <link rel="stylesheet" href="view/css/productos.css">
   <link rel="stylesheet" href="view/css-component/categoria.css">
   <link rel="stylesheet" href="view/css-component/favorito.css">
+
+  <script defer src="https://cdn.jsdelivr.net/npm/glider-js@1.7.9/glider.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/glider-js@1.7.9/glider.min.css" rel="stylesheet">
+  <script defer src="./view/js/carrusel-producto.js"></script>
 </head>
 <body>
-  <section class="productos dispositivo-Movil">
-    <header class="productos-cabecera">
-      <img src="view/icon/menu.png" alt="">
-      <p>Polleria Start</p>
-      <a href="index.php?opcion=carrito">
-        <img src="view/icon/carrito.png" alt="">
-      </a>
+<section class="CONTENEDOR-GENERAL">
+  <header class="header">
+    <h1>logo</h1>
+
+    <nav>
+      <a href="#" class="activo-nav">INICIO</a>
+      <a href="#">COMPRAS</a>
+      <div class="conteiner-user-perfil">
+        <img src="./view/img/perfil-user.jpg" alt="imagen del usuario">
+      </div>
+    </nav>
+  </header>
+
+  <article class="conteiner-carrusel">
+    <header class="header-carrusel">
+      <section>
+        <p>OFERTA</p>
+      </section>
+      <div id="resp-dots" class="indicador-carrusel" style="margin: 0px; padding: 0px; justify-content: end;"></div>
     </header>
-    <input class="productos-buscador" type="search" placeholder="buscar">
-    <div class="productos-ofertas">
-      <img src="view/img/oferta.jpg" alt="">
-    </div>
-    <section class="productos-conteiner-categorias">
-      <?php 
-        categoria('view/icon/plato.png','Platos');
-        categoria('view/icon/combo.png','Combos'); 
-        categoria('view/icon/bino.png','Binos'); 
-        categoria('view/icon/gaseosa.png','Gaseosas'); 
-        categoria('view/icon/refresco.png','Refrescos');
-      ?>
+    
+    <section>
+      <!-- fin carrosel -->
+      <div class="glider">
+        <img src="./view/img/pollo1.png" alt="">
+        <img src="./view/img/pollo2.png" alt="">
+        <img src="./view/img/pollo3.png" alt="">
+        <img src="./view/img/pollo4.png" alt="">
+        <img src="./view/img/pollo5.png" alt="">
+        <img src="./view/img/pollo6.png" alt="">
+        <img src="./view/img/pollo7.png" alt="">
+        <img src="./view/img/pollo8.png" alt="">
+      </div>
+      <!-- inicio carrosel -->
     </section>
-    <section class="productos-conteiner-favoritos">
-      <?php 
-        favorito('view/img/plato-de-comida.png', "Pollito a la brasa");
-        favorito('view/img/plato-de-comida.png', "Pollipapa");
-        favorito('view/img/plato-de-comida.png', "1/4 de Pollo");
-        favorito('view/img/plato-de-comida.png', "Alitas Picantes");
-        favorito('view/img/plato-de-comida.png', "Pollito a la brasa");
-        favorito('view/img/plato-de-comida.png', "Pollipapa");
-        favorito('view/img/plato-de-comida.png', "1/4 de Pollo");
-        favorito('view/img/plato-de-comida.png', "Alitas Picantes");
-      ?>
-    </section>
+  </article>
+
+  <section class="conteiner-categorias">
+    <a href="#" class="activo-categoria">categoria 1</a>
+    <a href="#">categoria 2</a>
+    <a href="#">categoria 3</a>
   </section>
+
+  <section>
+    <article></article>
+  </section>
+</section>
 </body>
